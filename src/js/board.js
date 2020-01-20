9ae89902a4a8a1bd7dd1eab1d5522accf483a911
Aditya Thebe <thebeaditya@gmail.com>
2020-01-20 10:18:48 +0545

    Added .prettierrc

--- src/js/board.js
+++ src/js/board.js
@@ -4,7 +4,11 @@ class Board {
     this.playerB = 'O';
     this.vacantCells = 9;
     this.currentPlayer = this.playerA;
-    this.grid = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    this.grid = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
 
     if (boardConfig) {
       this.grid = boardConfig.grid;
@@ -14,7 +18,11 @@ class Board {
   }
 
   resetBoard() {
-    this.grid = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    this.grid = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
   }
 
   getGrid() {
@@ -25,7 +33,10 @@ class Board {
     const vacantCells = [];
     for (let i = 0; i < 3; i += 1) {
       for (let j = 0; j < 3; j += 1) {
-        if (this.grid[i][j] !== this.playerA && this.grid[i][j] !== this.playerB) {
+        if (
+          this.grid[i][j] !== this.playerA &&
+          this.grid[i][j] !== this.playerB
+        ) {
           vacantCells.push([i, j]);
         }
       }
@@ -41,7 +52,11 @@ class Board {
   }
 
   validMove(x, y) {
-    return !this.isTerminalState() && this.grid[x][y] !== this.playerA && this.grid[x][y] !== this.playerB;
+    return (
+      !this.isTerminalState() &&
+      this.grid[x][y] !== this.playerA &&
+      this.grid[x][y] !== this.playerB
+    );
   }
 
   togglePlayer() {
@@ -53,20 +68,49 @@ class Board {
 
     for (const row of this.grid) {
       if (row[0] == row[1] && row[1] === row[2])
-        return { status: 'win', type: 'horizontal', winner: row[0], grid: this.getGrid() };
+        return {
+          status: 'win',
+          type: 'horizontal',
+          winner: row[0],
+          grid: this.getGrid(),
+        };
     }
 
     for (let i = 0; i < 3; i += 1) {
-      if (this.grid[0][i] == this.grid[1][i] && this.grid[1][i] == this.grid[2][i])
-        return { status: 'win', type: 'vertical', winner: this.grid[0][i], grid: this.getGrid() };
+      if (
+        this.grid[0][i] == this.grid[1][i] &&
+        this.grid[1][i] == this.grid[2][i]
+      )
+        return {
+          status: 'win',
+          type: 'vertical',
+          winner: this.grid[0][i],
+          grid: this.getGrid(),
+        };
     }
 
-    if (this.grid[0][0] == this.grid[1][1] && this.grid[1][1] == this.grid[2][2]) {
-      return { status: 'win', type: 'diagonal', winner: this.grid[1][1], grid: this.getGrid() };
+    if (
+      this.grid[0][0] == this.grid[1][1] &&
+      this.grid[1][1] == this.grid[2][2]
+    ) {
+      return {
+        status: 'win',
+        type: 'diagonal',
+        winner: this.grid[1][1],
+        grid: this.getGrid(),
+      };
     }
 
-    if (this.grid[0][2] == this.grid[1][1] && this.grid[1][1] == this.grid[2][0]) {
-      return { status: 'win', type: 'diagonal', winner: this.grid[1][1], grid: this.getGrid() };
+    if (
+      this.grid[0][2] == this.grid[1][1] &&
+      this.grid[1][1] == this.grid[2][0]
+    ) {
+      return {
+        status: 'win',
+        type: 'diagonal',
+        winner: this.grid[1][1],
+        grid: this.getGrid(),
+      };
     }
   }
 
